15cdf896baad16f47fd15752c3918110a9d6b543
raynato73@gmail.com <raynato73@gmail.com>
2023-05-19 06:30:26 +0700

    add navbar

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,12 @@
-export default function home(){
+import Navsbar from '@/component/Navsbar'
+
+export default function home() {
     return (
+
         <div>
+            <Navsbar>
             <h1>page home</h1>
+            </Navsbar>
         </div>
     )
 }
